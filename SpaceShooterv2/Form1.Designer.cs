e4f31613f00c4e9537c6a49cea3fa78cce36daeb
kent970 <123512406+kent970@users.noreply.github.com>
2023-05-18 15:40:54 +0200

    18 05 relaunch

--- SpaceShooterv2/Form1.Designer.cs
+++ SpaceShooterv2/Form1.Designer.cs
@@ -1,40 +1,159 @@
 ﻿namespace SpaceShooterv2
 {
-  partial class Form1
-  {
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-      if (disposing && (components != null))
-      {
-        components.Dispose();
-      }
-      base.Dispose(disposing);
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MoveBackground = new System.Timers.Timer();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.LeftMove = new System.Timers.Timer();
+            this.RightMove = new System.Timers.Timer();
+            this.UpMove = new System.Timers.Timer();
+            this.DownMove = new System.Timers.Timer();
+            this.MoveProtectile = new System.Timers.Timer();
+            this.MoveEnemy = new System.Timers.Timer();
+            this.EnemyProtectiles = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.MoveBackground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftMove)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightMove)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpMove)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DownMove)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MoveProtectile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MoveEnemy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyProtectiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MoveBackground
+            // 
+            this.MoveBackground.Enabled = true;
+            this.MoveBackground.Interval = 10D;
+            this.MoveBackground.SynchronizingObject = this;
+            // 
+            // Player
+            // 
+            this.Player.Image = ((System.Drawing.Image)(resources.GetObject("Player.Image")));
+            this.Player.Location = new System.Drawing.Point(265, 377);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(66, 68);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // LeftMove
+            // 
+            this.LeftMove.Enabled = true;
+            this.LeftMove.Interval = 5D;
+            this.LeftMove.SynchronizingObject = this;
+            this.LeftMove.Elapsed += new System.Timers.ElapsedEventHandler(this.LeftMove_Elapsed);
+            // 
+            // RightMove
+            // 
+            this.RightMove.Enabled = true;
+            this.RightMove.Interval = 5D;
+            this.RightMove.SynchronizingObject = this;
+            this.RightMove.Elapsed += new System.Timers.ElapsedEventHandler(this.RightMove_Elapsed);
+            // 
+            // UpMove
+            // 
+            this.UpMove.Enabled = true;
+            this.UpMove.Interval = 5D;
+            this.UpMove.SynchronizingObject = this;
+            this.UpMove.Elapsed += new System.Timers.ElapsedEventHandler(this.UpMove_Elapsed);
+            // 
+            // DownMove
+            // 
+            this.DownMove.Enabled = true;
+            this.DownMove.Interval = 5D;
+            this.DownMove.SynchronizingObject = this;
+            this.DownMove.Elapsed += new System.Timers.ElapsedEventHandler(this.DownMove_Elapsed);
+            // 
+            // MoveProtectile
+            // 
+            this.MoveProtectile.Enabled = true;
+            this.MoveProtectile.Interval = 20D;
+            this.MoveProtectile.SynchronizingObject = this;
+            this.MoveProtectile.Elapsed += new System.Timers.ElapsedEventHandler(this.MoveProtectile_Elapsed);
+            // 
+            // MoveEnemy
+            // 
+            this.MoveEnemy.Enabled = true;
+            this.MoveEnemy.SynchronizingObject = this;
+            this.MoveEnemy.Elapsed += new System.Timers.ElapsedEventHandler(this.MoveEnemy_Elapsed);
+            // 
+            // EnemyProtectiles
+            // 
+            this.EnemyProtectiles.Enabled = true;
+            this.EnemyProtectiles.Interval = 20D;
+            this.EnemyProtectiles.SynchronizingObject = this;
+            this.EnemyProtectiles.Elapsed += new System.Timers.ElapsedEventHandler(this.EnemyProtectiles_Elapsed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.Player);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.MoveBackground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftMove)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightMove)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpMove)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DownMove)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MoveProtectile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MoveEnemy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyProtectiles)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Timers.Timer EnemyProtectiles;
+
+        private System.Timers.Timer MoveEnemy;
+
+        private System.Timers.Timer MoveProtectile;
+
+        private System.Timers.Timer LeftMove;
+        private System.Timers.Timer RightMove;
+        private System.Timers.Timer UpMove;
+        private System.Timers.Timer DownMove;
+
+        private System.Windows.Forms.PictureBox Player;
+
+        private System.Timers.Timer MoveBackground;
+
+        #endregion
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
-    }
-
-    #endregion
-  }
 }
-
--- SpaceShooterv2/Form1.cs
+++ SpaceShooterv2/Form1.cs
@@ -6,15 +6,285 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace SpaceShooterv2
 {
-  public partial class Form1 : Form
-  {
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
+        private PictureBox[] _stars;
+        private PictureBox[] _protectiles;
+        private PictureBox[] _enemies;
+        private PictureBox[] _enemieProtectiles;
+        private int _backgroundspeed;
+        private Random _random;
+        private int _playerspeed;
+        private int _protectileSpeed;
+        private int _enemySpeed;
+        private int _enemyProtectileSpeed;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            var backgroundTimer = new System.Timers.Timer();
+            backgroundTimer.Interval = 20;
+            backgroundTimer.Elapsed += MoveBackground_Elapsed;
+            backgroundTimer.Start();
+
+
+            _backgroundspeed = 4;
+            _stars = new PictureBox[20];
+            _random = new Random();
+            _playerspeed = 4;
+            _protectileSpeed = 20;
+            _protectiles = new PictureBox[3];
+            _enemies = new PictureBox[10];
+            _enemySpeed = 20;
+            _enemyProtectileSpeed = 4;
+            
+            
+            // Image munition = Image.FromFile("asserts\\munition.png");
+            for (int i = 0; i < _protectiles.Length; i++)
+            {
+                _protectiles[i] = new PictureBox();
+                _protectiles[i].Size = new Size(8, 8);
+                //   protectiles[i].Image = munition;
+                _protectiles[i].BackColor = Color.Black;
+                _protectiles[i].SizeMode = PictureBoxSizeMode.Zoom;
+                _protectiles[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(_protectiles[i]);
+            }
+
+            for (int i = 0; i < _enemies.Length; i++)
+            {
+                _enemies[i] = new PictureBox();
+              //  _enemies[i].Image = new Bitmap(50, 50);
+              _enemies[i].BackColor = Color.IndianRed;
+              
+                _enemies[i].Size = new Size(50, 50);
+                _enemies[i].SizeMode = PictureBoxSizeMode.Zoom;
+               // _enemies[i].BorderStyle = BorderStyle.None;
+                _enemies[i].BorderStyle = BorderStyle.FixedSingle;
+                _enemies[i].Visible = false;
+                this.Controls.Add(_enemies[i]);
+                _enemies[i].Location = new Point((i + 1) * 50, -50);
+                
+            }
+
+
+            for (int i = 0; i < _stars.Length; i++)
+            {
+                _stars[i] = new PictureBox();
+                _stars[i].BorderStyle = BorderStyle.None;
+                _stars[i].Location = new Point(_random.Next(20, 580), _random.Next(-10, 400));
+                if (i % 2 == 1)
+                {
+                    _stars[i].Size = new Size(3, 3);
+                    _stars[i].BackColor = Color.Wheat;
+                }
+                else
+                {
+                    _stars[i].Size = new Size(4, 4);
+                    _stars[i].BackColor = Color.DarkBlue;
+                }
+
+                this.Controls.Add(_stars[i]);
+            }
+            
+            //enemy protectiles here
+
+            _enemieProtectiles = new PictureBox[10];
+            for (int i = 0; i < _enemieProtectiles.Length; i++)
+            {
+                _enemieProtectiles[i] = new PictureBox();
+                _enemieProtectiles[i].Size = new Size(2, 25);
+                _enemieProtectiles[i].Visible = false;
+                _enemieProtectiles[i].BackColor = Color.Yellow;
+                int x = _random.Next(0, 10);
+                _enemieProtectiles[i].Location = new Point(_enemies[x].Location.X, _enemies[x].Location.Y - 20);
+                this.Controls.Add(_enemieProtectiles[i]);
+            }
+        }
+
+        private void MoveBackground_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            for (int i = 0; i < _stars.Length / 2; i++)
+            {
+                _stars[i].Top += _backgroundspeed;
+                if (_stars[i].Top >= this.Height)
+                {
+                    _stars[i].Top = -_stars[i].Height;
+                }
+            }
+
+            for (int i = _stars.Length / 2; i < _stars.Length; i++)
+            {
+                _stars[i].Top += _backgroundspeed - 2;
+                if (_stars[i].Top >= this.Height)
+                {
+                    _stars[i].Top = -_stars[i].Height;
+                }
+            }
+        }
+
+        private void RightMove_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Player.Right < 550)
+            {
+                Player.Left += _playerspeed;
+            }
+        }
+
+        private void UpMove_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Player.Top > 10)
+            {
+                Player.Top -= _playerspeed;
+            }
+        }
+
+        private void LeftMove_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Player.Left > 10)
+            {
+                Player.Left -= _playerspeed;
+            }
+        }
+
+        private void DownMove_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Player.Top < 600)
+            {
+                Player.Top += _playerspeed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                RightMove.Start();
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                LeftMove.Start();
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                UpMove.Start();
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                DownMove.Start();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            RightMove.Stop();
+            LeftMove.Stop();
+            DownMove.Stop();
+            UpMove.Stop();
+        }
+
+
+        private void MoveProtectile_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            for (int i = 0; i < _protectiles.Length; i++)
+            {
+                if (_protectiles[i].Top > 0)
+                {
+                    _protectiles[i].Visible = true;
+                    _protectiles[i].Top -= _protectileSpeed;
+                    
+                    CheckCollision();
+                }
+                else
+                {
+                    _protectiles[i].Visible = false;
+                    _protectiles[i].Location = new Point(Player.Location.X + 20, Player.Location.Y - i * 30);
+                }
+            }
+        }
+
+        private void MoveEnemies(PictureBox[] enemies, int speed)
+        {
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].Visible = true;
+                enemies[i].Top += speed;
+
+                if (enemies[i].Top > this.Height)
+                {
+                    enemies[i].Location = new Point((i + 1) * 50, -200);
+                }
+
+                if (enemies[i].Location.Y == Player.Location.Y)
+                {
+                    this.BackColor=Color.Chartreuse;
+                }
+            
+            }
+
+        }
+
+        private void MoveEnemy_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            MoveEnemies(_enemies, _enemySpeed);
+        }
+
+        private void CheckCollision()
+        {
+            for (int i = 0; i < _enemies.Length; i++)
+            {
+                if ((_protectiles[0].Bounds.IntersectsWith(_enemies[i].Bounds)) ||
+                    (_protectiles[1].Bounds.IntersectsWith(_enemies[i].Bounds)) ||
+                    (_protectiles[2].Bounds.IntersectsWith(_enemies[i].Bounds)))
+                {
+                    _enemies[i].Location = new Point((i + 1) * 50, -100);
+                }
+
+                if (Player.Bounds.IntersectsWith(_enemies[i].Bounds))
+                {
+                    Player.Visible = false;
+                    GameOver("Game Over");
+                }
+            }
+        }
+
+        private void GameOver(string str)
+        {
+            MoveEnemy.Stop();
+            MoveProtectile.Stop();
+            
+        }
+
+
+        private void EnemyProtectiles_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            for (int i = 0; i < _enemieProtectiles.Length; i++)
+            {
+                if (_enemieProtectiles[i].Top < this.Height)
+                {
+                    _enemieProtectiles[i].Visible = true;
+                    _enemieProtectiles[i].Top += _enemyProtectileSpeed;
+                }
+                else
+                {
+                    _enemieProtectiles[i].Visible = false;
+                    int x = _random.Next(0, 10);
+                    _enemieProtectiles[i].Location =
+                        new Point(_enemies[x].Location.X + 20, _enemies[x].Location.Y + 30);
+                }
+            }
+        }
     }
-  }
 }
